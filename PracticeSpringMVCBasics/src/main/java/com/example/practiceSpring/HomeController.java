95c15c9dd2967681952e66df2a23b69a27963745
suprajaLara <17311A0587@sreenidhi.edu.in>
2021-05-14 22:19:24 +0530

    adding new app

---
+++ PracticeSpringMVCBasics/src/main/java/com/example/practiceSpring/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.practiceSpring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String showPage(){
+        return "mainMenu.html";
+    }
+}
